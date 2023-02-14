2fd87bdeba119206b0ec858460bd6b11d2e9255d
Alex Nguyen <hoangminh98@gmail.com>
2023-02-14 02:22:16 -0800

    Add helmet middleware

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,7 @@
 import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
 
 dotenv.config();
 const PORT: number = parseInt(process.env.PORT as string, 10);
@@ -9,6 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(helmet());
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello World" });
