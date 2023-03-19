2f4e2259608caa8242f54e7b721f3d065acf3dab
Gurman17 <chauhangurman@gmail.com>
2023-03-18 17:11:33 -0700

    fix vertical margins

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -4,9 +4,9 @@ import SignupForm from "components/SignupForm";
 
 const Signup = () => {
   return (
-    <Box w="40%" mx="auto">
-      <Heading mb="20px">Signup</Heading>
-      <Link href="/login" display="block" mb="20px">
+    <Box w="40%" mx="auto" mb="30px">
+      <Heading>Signup</Heading>
+      <Link href="/login" display="block" my="30px">
         <Text as="u">Already have an account? Sign in</Text>
       </Link>
       <SignupForm />
