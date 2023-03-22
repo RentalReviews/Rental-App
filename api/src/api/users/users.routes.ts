671bb14c09f0c2b8cd242cd7032c176065342d54
Alex Nguyen <hoangminh98@gmail.com>
2023-03-22 16:35:17 -0700

    api: Fix subtitution issue with GH Actions

--- api/src/api/users/users.routes.ts
+++ api/src/api/users/users.routes.ts
@@ -4,6 +4,7 @@ import { GetUserById, GetUserByEmail } from "./users.controllers";
 
 const router = express.Router();
 
+// regex for UUID
 const idRegex =
   "[0-9a-fA-F]{8}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{12}";
 const emailRegex = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}";
