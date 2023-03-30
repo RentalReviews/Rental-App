273ca0a5a003ff91ab439ec79fa07470ac74148b
Gurman17 <chauhangurman@gmail.com>
2023-03-29 17:07:35 -0700

    add redux store

---
+++ client/src/store.ts
@@ -0,0 +1,26 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: null,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUser(state, action) {
+      state.user = action.payload;
+    },
+    clearUser(state) {
+      state.user = null;
+    },
+  },
+});
+
+const store = configureStore({
+  reducer: {
+    user: userSlice.reducer,
+  },
+});
+
+export default store;
