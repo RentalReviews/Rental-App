e350af3754936692168cf34d88a126862c8d6760
Gurman17 <chauhangurman@gmail.com>
2023-03-31 17:01:18 -0700

    fix initialization in user store

--- client/src/redux/user.ts
+++ client/src/redux/user.ts
@@ -1,10 +1,34 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+interface UserState {
+  user?: {
+    id: string;
+    displayName: string;
+    email: string;
+    refreshToken: string;
+    bearerToken: string;
+  } | null;
+}
+
+const userInfo = JSON.parse(localStorage.getItem("USER") || "{}");
+const refreshToken = localStorage.getItem("REFRESH_TOKEN");
+const bearerToken = localStorage.getItem("BEARER_TOKEN");
+
+const initialState: UserState = {
+  user: null,
+};
+
+if (userInfo && refreshToken && bearerToken) {
+  initialState.user = {
+    ...userInfo,
+    refreshToken: refreshToken,
+    bearerToken: bearerToken,
+  };
+}
+
 const userSlice = createSlice({
   name: "user",
-  initialState: {
-    user: null,
-  },
+  initialState,
   reducers: {
     setUser(state, action) {
       state.user = action.payload;
@@ -14,6 +38,8 @@ const userSlice = createSlice({
     },
   },
 });
+
+export const userSelector = (state: { user: UserState }) => state.user;
 
 export const { setUser, clearUser } = userSlice.actions;
 
