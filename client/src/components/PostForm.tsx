79713a5a5956e2e1f54dedbdb21dce2f8e17a960
Alex Nguyen <hoangminh98@gmail.com>
2023-03-31 19:03:43 -0700

    client: Refactor and clean up PostForm and UserHome

--- client/src/components/PostForm.tsx
+++ client/src/components/PostForm.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction } from "react";
+import { useState } from "react";
 import {
   Accordion,
   AccordionButton,
@@ -7,18 +7,63 @@ import {
   Box,
   Button,
   Input,
+  useToast,
 } from "@chakra-ui/react";
 import { MinusIcon, AddIcon } from "@chakra-ui/icons";
+import { useNavigate } from "react-router-dom";
+import { genericErrorHandler } from "utils";
 
 import type { Post } from "types";
 
-interface props {
-  post: Post;
-  setPost: Dispatch<SetStateAction<Post>>;
-  updatePosts: () => void;
-}
+const API_URL = `${import.meta.env.VITE_API_SERVER_URL}/api/v1`;
 
-export const PostForm = (props: props) => {
+export const PostForm = (props: { post?: Post }) => {
+  const IS_EDITING = props.post !== undefined;
+  const AuthToken = localStorage.getItem("BEARER_TOKEN") || "";
+
+  const [formState, setFormState] = useState({
+    title: props.post?.title || "",
+    content: props.post?.content || "",
+    rating: props.post?.rating || 1,
+    imageUrl: props.post?.postPhotos[0]?.url || "",
+  });
+  const navigate = useNavigate();
+  const toast = useToast();
+
+  const createPost = async () => {
+    if (!AuthToken) return navigate("/login");
+    if (formState.title === "" || formState.content === "") return;
+
+    try {
+      const response = await fetch(`${API_URL}/postings`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${AuthToken}`,
+        },
+        body: JSON.stringify({
+          title: formState.title,
+          content: formState.content,
+          rating: formState.rating,
+          postPhotos: [{ url: formState.imageUrl }],
+        }),
+      });
+      const json = await response.json();
+      if (response.ok) {
+        navigate(0);
+      } else {
+        toast({
+          title: "Error creating post",
+          description: json.message || "Something went wrong.",
+          status: "error",
+          duration: 3000,
+        });
+      }
+    } catch (err) {
+      genericErrorHandler(err, toast);
+    }
+  };
+
   return (
     <>
       <Accordion allowMultiple>
@@ -39,102 +84,79 @@ export const PostForm = (props: props) => {
                 </AccordionButton>
               </h2>
               <AccordionPanel pb={4}>
-                <form
-                  onSubmit={(e) => {
-                    e.preventDefault();
-                    props.updatePosts();
-                  }}
-                >
+                <form>
                   <div>
-                    <label htmlFor="">Rental Address: </label>
+                    <label htmlFor="title">Title: </label>
                     <Input
+                      name="title"
                       type="text"
-                      onChange={(e) =>
-                        props.setPost({
-                          authorId: props.post.authorId,
-                          comments: props.post.comments,
-                          content: props.post.content,
-                          createdAt: props.post.createdAt,
-                          id: props.post.id,
-                          postPhotos: props.post.postPhotos,
-                          published: props.post.published,
+                      value={formState.title}
+                      onChange={(e) => {
+                        setFormState({
+                          ...formState,
                           title: e.target.value,
-                          updatedAt: props.post.updatedAt,
-                          rating: props.post.rating,
-                        })
-                      }
+                        });
+                      }}
                       required
                     />
                   </div>
                   <div>
-                    <label htmlFor="">Image URL: </label>
+                    <label htmlFor="image-url">Image URL: </label>
                     <Input
                       type="text"
-                      name=""
-                      id=""
-                      onChange={(e) =>
-                        props.setPost({
-                          authorId: props.post.authorId,
-                          comments: props.post.comments,
-                          content: props.post.content,
-                          createdAt: props.post.createdAt,
-                          id: props.post.id,
-                          postPhotos: [{ url: e.target.value }],
-                          published: props.post.published,
-                          title: props.post.title,
-                          updatedAt: props.post.updatedAt,
-                          rating: props.post.rating,
-                        })
-                      }
+                      name="image-url"
+                      value={formState.imageUrl}
+                      onChange={(e) => {
+                        setFormState({
+                          ...formState,
+                          imageUrl: e.target.value,
+                        });
+                      }}
                       required
                     />
                   </div>
                   <div>
-                    <label htmlFor="">Rating: </label>
+                    <label htmlFor="rating">Rating: </label>
                     <Input
                       type="number"
-                      name=""
-                      id=""
-                      onChange={(e) =>
-                        props.setPost({
-                          authorId: props.post.authorId,
-                          comments: props.post.comments,
-                          content: e.target.value,
-                          createdAt: props.post.createdAt,
-                          id: props.post.id,
-                          postPhotos: props.post.postPhotos,
-                          published: props.post.published,
-                          title: props.post.title,
-                          updatedAt: props.post.updatedAt,
+                      name="rating"
+                      value={formState.rating}
+                      min={1}
+                      max={5}
+                      onChange={(e) => {
+                        setFormState({
+                          ...formState,
                           rating: Number(e.target.value),
-                        })
-                      }
+                        });
+                      }}
                       required
                     />
                   </div>
                   <div>
-                    <label htmlFor="">Caption: </label>
+                    <label htmlFor="content">Content: </label>
                     <Input
                       type="text"
-                      name=""
-                      id=""
-                      onChange={(e) =>
-                        props.setPost({
-                          authorId: props.post.authorId,
-                          comments: props.post.comments,
+                      name="content"
+                      value={formState.content}
+                      onChange={(e) => {
+                        setFormState({
+                          ...formState,
                           content: e.target.value,
-                          createdAt: props.post.createdAt,
-                          id: props.post.id,
-                          postPhotos: props.post.postPhotos,
-                          published: props.post.published,
-                          title: props.post.title,
-                          updatedAt: props.post.updatedAt,
-                          rating: props.post.rating,
-                        })
-                      }
+                        });
+                      }}
+                      required
                     />
                   </div>
-                  <Button type="submit">OK</Button>
+                  <Button
+                    onClick={(e) => {
+                      e.preventDefault();
+                      if (!IS_EDITING) {
+                        createPost();
+                      }
+                    }}
+                  >
+                    Submit
+                  </Button>
                 </form>
               </AccordionPanel>
             </>
--- client/src/pages/UserHome.tsx
+++ client/src/pages/UserHome.tsx
@@ -1,28 +1,16 @@
-import { Heading, Wrap, WrapItem, useToast } from "@chakra-ui/react";
-import Posting from "components/Posting";
-
-import { PostForm } from "components/PostForm";
+import { Heading, Wrap, WrapItem } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
-import { Post } from "types/Post";
 import jwt_decode from "jwt-decode";
-import { RefreshToken } from "types/RefreshToken";
-import { genericErrorHandler } from "utils";
+
+import Posting from "components/Posting";
+import { PostForm } from "components/PostForm";
+
+import type { Post } from "types/Post";
+import type { RefreshToken } from "types/RefreshToken";
+
 const API_URL = `${import.meta.env.VITE_API_SERVER_URL}/api/v1`;
 
 const Home = () => {
-  const toast = useToast();
-  const [post, setPost] = useState<Post>({
-    authorId: "",
-    comments: [],
-    content: "",
-    createdAt: new Date(),
-    id: "",
-    postPhotos: [],
-    published: false,
-    updatedAt: new Date(),
-    title: "",
-    rating: 0,
-  });
   const [posts, setPosts] = useState<Post[]>([]);
   const REFRESH_TOKEN: RefreshToken | string | null = localStorage.getItem("REFRESH_TOKEN") || "";
   let decoded: RefreshToken | undefined = undefined;
@@ -34,24 +22,6 @@ const Home = () => {
       ? (decoded.exp ? decoded.exp : Number.MAX_SAFE_INTEGER) > (new Date().getTime() + 1) / 1000
       : false
   );
-
-  const updatePosts = () => {
-    setPosts([...posts, post]);
-    console.log(post);
-    postReview(post);
-    setPost({
-      authorId: "",
-      comments: [],
-      content: "",
-      createdAt: new Date(),
-      id: "",
-      postPhotos: [],
-      published: false,
-      updatedAt: new Date(),
-      title: "",
-      rating: 0,
-    });
-  };
 
   useEffect(() => {
     getAll().then((data) => {
@@ -78,75 +48,19 @@ const Home = () => {
     }
   };
 
-  const postReview = async (post: Post) => {
-    const token = "Bearer " + localStorage.getItem("BEARER_TOKEN")?.toString();
-    try {
-      await fetch(`${API_URL}/postings`, {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          Authorization: token,
-        },
-        body: JSON.stringify({
-          // Can't input the ID for the post, it's generated on server side
-          authorId: post.authorId,
-          comments: post.comments,
-          content: post.content,
-          createdAt: post.createdAt,
-          id: "Does this autogenerate",
-          postPhotos: post.postPhotos,
-          published: post.published,
-          updatedAt: post.updatedAt,
-          title: post.title,
-          rating: post.rating,
-        }),
-      }).then((response) => {
-        if (response.ok) {
-          addPostToUI();
-        }
-      });
-    } catch (err) {
-      genericErrorHandler(err, toast);
-    }
-  };
-
-  const addPostToUI = (): void => {
-    setPosts([post, ...posts]);
-  };
-
-  const removePostFromUI = (postId: string | undefined): void => {
-    const newPostsArray = posts.filter((comment) => comment.id !== postId);
-    setPosts(newPostsArray);
-  };
-
   return (
     <>
       <Heading textAlign="center" noOfLines={1} mb={3}>
         Home - {isOnline ? "Online" : "Offline"}
       </Heading>
-      {isOnline && <PostForm post={post} setPost={setPost} updatePosts={updatePosts} />}
+      {isOnline && <PostForm />}
       <div id="posts">
         <div id="posts">
           <Wrap>
             {posts.map((post, i) => {
               return (
                 <WrapItem key={i}>
-                  <Posting
-                    key={i}
-                    post={{
-                      authorId: post.authorId,
-                      comments: post.comments,
-                      content: post.content,
-                      createdAt: post.createdAt,
-                      id: post.id,
-                      postPhotos: post.postPhotos,
-                      published: post.published,
-                      updatedAt: post.updatedAt,
-                      title: post.title,
-                      rating: post.rating,
-                    }}
-                  />
+                  <Posting key={i} post={post} />
                 </WrapItem>
               );
             })}
